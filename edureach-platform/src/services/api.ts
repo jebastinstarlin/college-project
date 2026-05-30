ed505b5e0df18724c5b896c76e8213985c6f5db1
Jebastin Starlin <jebastinstarlin@gmail.com>
2026-05-30 13:16:18 +0530

    add node

--- edureach-platform/src/services/api.ts
+++ edureach-platform/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",                 // Localhost
+  // baseURL: "https://edureach-backend.onrender.com/api",    // Render
+
 });
 
 API.interceptors.request.use((config) => {
